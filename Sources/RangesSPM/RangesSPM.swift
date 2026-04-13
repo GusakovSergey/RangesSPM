7420f35ce08773b01b8ac97205b1f2328377b5d6
segusakov <segusakov@ozon.ru>
2026-04-13 12:17:14 +0300

    initial commib

---
+++ Sources/RangesSPM/RangesSPM.swift
@@ -0,0 +1,10 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Alamofire
+
+public class MyClass {
+    public let prop = Alamofire.AFError.explicitlyCancelled
+    
+    public init() {}
+}
